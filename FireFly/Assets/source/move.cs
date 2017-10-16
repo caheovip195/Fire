6d6297b71e08c4063371a1c2a17e862d5a6cba3d
Nguyen Cong <ntcong95.oplai@gmail.com>
2017-10-16 15:42:46 +0700

    update master

---
+++ FireFly/Assets/source/move.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour {
+    float x, y;
+
+	// Use this for initialization
+	void Start () {
+        x = this.transform.position.x;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 mo = new Vector3(x + 1, 0, 0);
+        this.transform.Translate(mo);
+        x = this.transform.position.x;
+	}
+}
